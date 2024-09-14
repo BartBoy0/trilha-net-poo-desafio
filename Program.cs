225d22eadd21d9935756121c8755dc96af8423d0
BartBoy0 <alexvinicius0001@gmail.com>
2024-09-14 07:57:27 -0300

    Completando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("123","1","11111111","1");
+iphone.Ligar();
+Nokia nokia = new Nokia("123","1","11111111","1");
+nokia.ReceberLigacao();
+
+iphone.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Instagram");
